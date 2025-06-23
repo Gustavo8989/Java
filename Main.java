96d12737dea00b55b4cc442ccea62692b66b4026
Gustavo <gustavohenriquealves418@gmail.com>
2025-06-23 17:47:17 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner; 
+
+public class Main {
+    public static void main(String[] args){
+        // Teste comentario 
+        Scanner scanner; 
+        System.out.println("Olá Mundo");
+
+    }
+}
