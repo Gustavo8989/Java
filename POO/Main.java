3b284f399546f93a99e230b0f047727a3f0d1bf7
Gustavo <gustavohenriquealves418@gmail.com>
2025-07-14 18:39:18 -0300

    POO

---
+++ POO/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+
+    public static void main(String[] args){
+        var male = new Person();
+        male.name = "joão";
+        male.age = 18;
+        var female = new Person();
+        female.name = "Isa";
+        female.age = 17;
+        
+    }
+}
